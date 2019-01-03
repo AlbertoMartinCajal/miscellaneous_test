614010f04bf090453b6a892b0e879a835234fed3
AlbertoMartinCajal <alberto.martincajal@gmail.com>
2019-01-04 00:53:00 +0100

    Try to install more packages

--- install-sw.sh
+++ install-sw.sh
@@ -2,10 +2,13 @@
 
 sudo pip install --upgrade pip
 sudo pip install flawfinder
-sudo apt-get install gdb
+
+sudo apt-get update
+sudo apt-get install build-essential
 sudo apt-get install libpng-dev
 sudo apt-get install zlib1g-dev
 sudo apt-get install cppcheck
+
 wget http://downloads.sourceforge.net/ltp/lcov-1.11.tar.gz;
 tar xvfz lcov-1.11.tar.gz;
 make -C lcov-1.11;
