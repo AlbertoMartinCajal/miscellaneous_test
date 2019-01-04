ea3e083f6200408f0f267e42b36ce9bb7dc44fa2
AlbertoMartinCajal <alberto.martincajal@gmail.com>
2019-01-04 02:29:05 +0100

    Update install-sw.sh

--- install-sw.sh
+++ install-sw.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo pip install --upgrade pip
-sudo pip install flawfinder
+yes | sudo pip install --upgrade pip
+yes | sudo pip install flawfinder
 
-sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install libpng-dev
-sudo apt-get install zlib1g-dev
-sudo apt-get install cppcheck
+sudo apt-get update -y
+sudo apt-get install -ybuild-essential
+sudo apt-get install -y libpng-dev
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y cppcheck 
 
 wget http://downloads.sourceforge.net/ltp/lcov-1.11.tar.gz;
 tar xvfz lcov-1.11.tar.gz;
